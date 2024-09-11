ed7fd7da5528a7f47f10e1c6cbdaa14312814f07
nguyenquoctinh <nguyentinh140321@gmail.com>
2024-09-11 10:26:01 +0700

    seeder Data

--- database/seeders/CargoCarTypeSeeder.php
+++ database/seeders/CargoCarTypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CargoCarTypeSeeder extends Seeder
 {
@@ -12,6 +13,36 @@ class CargoCarTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $hardData = [
+            [
+                'name' => 'Xe tải nhỏ',
+                'capacity' => '2',
+            ],
+            [
+                'name' => 'Xe tải trung',
+                'capacity' => '5',
+            ],
+            [
+                'name' => 'Xe tải lớn',
+                'capacity' => '10',
+            ],
+            [
+                'name' => 'Xe container 20ft',
+                'capacity' => '20',
+            ],
+            [
+                'name' => 'Xe container 40ft',
+                'capacity' => '30',
+            ],
+        ];
+        $id = 1;
+        foreach ($hardData as $value) {
+          DB::table('cargo_car_types')->insert([
+             'id' =>  $id,
+            'name' =>  $value['name'],
+            'capacity' =>  $value['capacity'],
+        ]);
+        $id++;
+    }
     }
 }
--- database/seeders/ContractTypeSeeder.php
+++ database/seeders/ContractTypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ContractTypeSeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class ContractTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('contract_types')->insert([
+            ['id' => 1, 'name' => 'Hợp đồng thanh toán', 'description' => 'Hợp đồng thanh toán Online'],
+            ['id' => 2, 'name' => 'Hợp đồng thanh toán', 'description' => 'Hợp đồng thanh toán Offline'],
+            ['id' => 3, 'name' => 'Hợp đồng thi công', 'description' => 'Hợp đồng liên quan đến việc thi công công trình xây dựng.'],
+            ['id' => 4, 'name' => 'Hợp đồng cung cấp vật tư', 'description' => 'Hợp đồng liên quan đến việc cung cấp vật tư xây dựng cho dự án.'],
+            ['id' => 5, 'name' => 'Hợp đồng thuê thiết bị', 'description' => 'Hợp đồng thuê thiết bị xây dựng cho công trình.'],
+            ['id' => 6, 'name' => 'Hợp đồng bảo trì', 'description' => 'Hợp đồng bảo trì và sửa chữa công trình xây dựng.'],
+            ['id' => 7, 'name' => 'Hợp đồng tư vấn', 'description' => 'Hợp đồng liên quan đến dịch vụ tư vấn xây dựng và kỹ thuật.'],
+
+        ]);
     }
 }
