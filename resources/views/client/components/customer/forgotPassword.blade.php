19fa0d72280cbf16e17b6fa0b4f3d71328c5f86b
quangdangcode <quangdangcode@gmail.com>
2024-10-12 00:28:23 +0700

    quang

--- resources/views/client/components/customer/forgotPassword.blade.php
+++ resources/views/client/components/customer/forgotPassword.blade.php
@@ -1,33 +1,122 @@
-@extends('admin.layouts.master')
+@extends('client.layouts.master')
 
 @section('title')
     Quên mật khẩu
 @endsection
 
 @section('content')
-
-
-    <form action="{{ route('sendMaill') }}" method="post">
-        @csrf
+    <!-- Breadcrumb Start -->
+    <div class="breadcrumb-area ptb-60 ptb-sm-30">
         <div class="container">
-            <h1>Quên mật khẩu</h1>
-            <p>Vui lòng điền vào mẫu này để Lấy lại mật khẩu.</p>
-            <hr>
-            <label for="email"><b>Email</b></label><br>
-            <input type="text" class="form-control w-50" placeholder="Email" name="email" id="email">
-            @error('email')
-                <label for="" class="text-danger">{{ $message }}</label>
-            @enderror
-            <hr>
-            <button type="submit" class="btn btn-primary">Lấy lại mật khẩu</button>
-        </div><br>
-        <div class="container signin">
-            <p>Nếu bạn chưa có tài khoản: <a href="{{ route('register') }}">Đăng ký</a>.</p>
+            <div class="breadcrumb">
+                <ul>
+                    <li><a href="index.html">Trang chủ</a></li>
+                    <li class="active"><a href="register.html">Quên mật khẩu</a></li>
+                </ul>
+            </div>
         </div>
-        <div class="container signin">
-            <p>Nếu bạn đã có tài khoản: <a href="{{ route('login') }}">Đăng nhập</a>.</p>
+        <!-- Container End -->
+    </div>
+    <!-- Breadcrumb End -->
+    <!-- Register Account Start -->
+    <div class="register-account pb-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="register-title">
+                        <h3 class="mb-10">Quên mật khẩu</h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Row End -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <form class="form-horizontal" action="{{ route('client.sendMaill') }}" method="post">
+                        @csrf
+                        <fieldset>
+                            <legend>Vui lòng điền vào mẫu này để lấy lại mật khẩu</legend>
+                            <div class="form-group">
+                                <label class="control-label" for="f-name"><span class="require">*</span>Email</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control w-50 mt-2"
+                                        placeholder="Nhập địa chỉ email của bạn vào đây..." name="email" id="email">
+                                    @error('email')
+                                        <label for="" class="text-danger">{{ $message }}</label>
+                                    @enderror
+                                </div>
+                            </div>
+                        </fieldset>
+                        <div class="buttons newsletter-input">
+                            {{-- <div class="pull-right">I have read and agree to the <a href="#" class="agree"><b>Privacy
+                                        Policy</b></a> --}}
+                            <div class="pull-left">
+                                {{-- <input type="checkbox" name="agree" value="1"> &nbsp; --}}
+                                <input type="submit" value="Lấy lại mật khẩu" class="newsletter-btn">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="container signin">
+                    <p class="mt-3">Nếu bạn đã có tài khoản hãy: <a href="{{ route('client.login') }}"
+                            class="active ">Đăng Nhập</a>.</p>
+                </div>
+                <div class="container signin">
+                    <p>Nếu bạn chưa có tài khoản hãy: <a href="{{ route('client.register') }}">Đăng ký</a>.</p>
+                </div>
+            </div>
+            <!-- Row End -->
         </div>
-    </form>
-
-    
+        <!-- Container End -->
+    </div>
+    <!-- Register Account End -->
+    <!-- Brand Logo Start -->
+    <div class="brand-area pb-60">
+        <div class="container">
+            <!-- Brand Banner Start -->
+            <div class="brand-banner owl-carousel">
+                <div class="single-brand">
+                    <a href="#"><img class="img" src="{{ asset('themes/client/template/img/brand/1.png') }}"
+                            alt="brand-image"></a>
+                </div>
+                <div class="single-brand">
+                    <a href="#"><img src="{{ asset('themes/client/template/img/brand/2.png') }}"
+                            alt="brand-image"></a>
+                </div>
+                <div class="single-brand">
+                    <a href="#"><img src="{{ asset('themes/client/template/img/brand/3.png') }}"
+                            alt="brand-image"></a>
+                </div>
+                <div class="single-brand">
+                    <a href="#"><img src="{{ asset('themes/client/template/img/brand/4.png') }}"
+                            alt="brand-image"></a>
+                </div>
+                <div class="single-brand">
+                    <a href="#"><img src="{{ asset('themes/client/template/img/brand/5.png') }}"
+                            alt="brand-image"></a>
+                </div>
+                <div class="single-brand">
+                    <a href="#"><img class="img" src="{{ asset('themes/client/template/img/brand/1.png') }}"
+                            alt="brand-image"></a>
+                </div>
+                <div class="single-brand">
+                    <a href="#"><img src="{{ asset('themes/client/template/img/brand/2.png') }}"
+                            alt="brand-image"></a>
+                </div>
+                <div class="single-brand">
+                    <a href="#"><img src="{{ asset('themes/client/template/img/brand/3.png') }}"
+                            alt="brand-image"></a>
+                </div>
+                <div class="single-brand">
+                    <a href="#"><img src="{{ asset('themes/client/template/img/brand/4.png') }}"
+                            alt="brand-image"></a>
+                </div>
+                <div class="single-brand">
+                    <a href="#"><img src="{{ asset('themes/client/template/img/brand/5.png') }}"
+                            alt="brand-image"></a>
+                </div>
+            </div>
+            <!-- Brand Banner End -->
+        </div>
+    </div>
+    <!-- Brand Logo End -->
 @endsection
