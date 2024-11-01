0165a16823e5016856f7ec3040b67cc95dd00861
Nguyễn Quốc Tình <125818904+nguyentinhquoc@users.noreply.github.com>
2024-11-01 16:25:53 +0700

    Tinh Update

--- app/Http/Controllers/PermissionRoleEmployeesController.php
+++ app/Http/Controllers/PermissionRoleEmployeesController.php
@@ -12,20 +12,40 @@ class PermissionRoleEmployeesController extends Controller
     /**
      * Display a listing of the resource.
      */
-
-
-
     public function index()
     {
-        //
+        $permission_role_employees = DB::table("permission_role_employees")->get();
+        $permissions = DB::table("permissions")->get();
+        $role_employees = DB::table("role_employees")->get();
+       return view('admin.components.permissions.home-permissions',compact('permissions', 'role_employees', 'permission_role_employees'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function permissionsToggle()
     {
-        //
+        $role_id = request('role_id');
+        $permission_id = request('permission_id');
+
+        $exists = DB::table('permission_role_employees')
+            ->where('role_employee_id', $role_id)
+            ->where('permission_id', $permission_id)
+            ->exists();
+
+        if ($exists) {
+            DB::table('permission_role_employees')
+                ->where('role_employee_id', $role_id)
+                ->where('permission_id', $permission_id)
+                ->delete();
+        } else {
+            DB::table('permission_role_employees')->insert([
+                'role_employee_id' => $role_id,
+                'permission_id' => $permission_id
+            ]);
+        }
+
+        return response()->json(['success' => true]);
     }
 
     /**
---
+++ resources/views/admin/components/permissions/home-permissions.blade.php
@@ -0,0 +1,84 @@
+@extends('admin.layouts.master')
+
+
+@section('title')
+    Phân quyền
+@endsection
+@section('content')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                <h4 class="mb-sm-0">Phân quyền</h4>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header border-0">
+                    <div class="row g-4">
+                        <div class="col-sm-auto">
+                            <div>
+                                <a class="btn btn-success" id="addpermission-btn"><i
+                                        class="ri-add-line align-bottom me-1"></i>Thêm chức vụ</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <table id="myTable" class="table table-bordered dt-responsive nowrap table-striped align-middle"
+                        style="width:100%">
+                        <thead>
+                            <tr>
+                                <th data-ordering="false">Phân quyền</th>
+                               @foreach ($role_employees as $role_employee)
+                                <th>{{ $role_employee->name }}</th>
+                               @endforeach
+                            </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($permissions as $permission)
+                          <tr>
+                            <td>{{ $permission->name }}</td>
+                            @foreach($role_employees as $role_employee)
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input  class="form-check-input" type="checkbox"
+                                           onchange="checkTest({{ $role_employee->id }}, {{ $permission->id }}, '{{ $role_employee->name }}', '{{ $permission->name }}')"
+                                           {{ $permission_role_employees->where('permission_id', $permission->id)->where('role_employee_id', $role_employee->id)->count() > 0 ? 'checked' : '' }}
+                                           onchange="togglePermission({{ $permission->id }}, {{ $role_employee->id }}, this)"
+                                           {{ $role_employee->id == 1 ? 'disabled' : '' }}>
+                                </div>
+                            </td>
+                            @endforeach
+                          </tr>
+                          @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        function checkTest(role, permission, roleName, permissionName) {
+            let isChecked = event.target.checked;
+            let message = isChecked ?
+                `Bạn có muốn thêm quyền ${permissionName} cho ${roleName} ?` :
+                `Bạn có muốn xóa quyền ${permissionName} của ${roleName} ?`;
+            if (confirm(message)) {
+                $.ajax({
+                    url: '/permissions/toggle',
+                    type: 'POST',
+                    data: {
+                        role_id: role,
+                        permission_id: permission,
+                        _token: '{{ csrf_token() }}'
+                    },
+                });
+            } else {
+                event.target.checked = !isChecked;
+            }
+        }
+    </script>
+@endsection
