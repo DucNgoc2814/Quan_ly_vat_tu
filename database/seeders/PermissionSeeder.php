fccaaf6d71581a381de3daeb1f52ba10fdae779a
Nguyễn Quốc Tình <125818904+nguyentinhquoc@users.noreply.github.com>
2024-11-01 21:18:29 +0700

    Tinh Hoan thien phan quyen

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -14,307 +14,329 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         DB::table(table: 'permissions')->insert([
-            // Quản lý nhà phân phối
+            // Dashboard
             [
                 'id' => 1,
-                'name' => 'Xem danh sách nhà phân phối',
+                'name' => 'Xem trang dashboard quản trị',
             ],
+
+            // Quản lý nhân viên và phân quyền
             [
                 'id' => 2,
-                'name' => 'Xem danh sách nhà phân phối đã ẩn',
+                'name' => 'Thêm chức vụ nhân viên',
             ],
             [
                 'id' => 3,
-                'name' => 'Khôi phục nhà phân phối',
+                'name' => 'Cập nhật quyền truy cập',
             ],
             [
                 'id' => 4,
-                'name' => 'Thêm mới nhà phân phối',
+                'name' => 'Xem danh sách quyền truy cập',
             ],
             [
                 'id' => 5,
-                'name' => 'Sửa nhà phân phối',
-            ],
-            [
-                'id' => 6,
-                'name' => 'Ẩn nhà phân phối',
-            ],
-
-            // Quản lý nhân viên
-            [
-                'id' => 7,
                 'name' => 'Xem danh sách nhân viên',
             ],
             [
-                'id' => 8,
+                'id' => 6,
                 'name' => 'Thêm mới nhân viên',
             ],
             [
-                'id' => 9,
+                'id' => 7,
                 'name' => 'Sửa thông tin nhân viên',
+            ],
+            [
+                'id' => 8,
+                'name' => 'Cập nhật thông tin nhân viên',
+            ],
+
+            // Quản lý nhà phân phối
+            [
+                'id' => 9,
+                'name' => 'Xem danh sách nhà phân phối',
+            ],
+            [
+                'id' => 10,
+                'name' => 'Xem danh sách nhà phân phối đã ẩn',
+            ],
+            [
+                'id' => 11,
+                'name' => 'Khôi phục nhà phân phối',
+            ],
+            [
+                'id' => 12,
+                'name' => 'Thêm mới nhà phân phối',
+            ],
+            [
+                'id' => 13,
+                'name' => 'Sửa nhà phân phối',
+            ],
+            [
+                'id' => 14,
+                'name' => 'Ẩn nhà phân phối',
             ],
 
             // Quản lý bán hàng
             [
-                'id' => 10,
-                'name' => 'Xem danh sách đơn bán',
+                'id' => 15,
+                'name' => 'Xem danh sách đơn hàng bán',
             ],
             [
-                'id' => 11,
-                'name' => 'Thêm đơn hàng',
+                'id' => 16,
+                'name' => 'Thêm đơn hàng mới',
             ],
             [
-                'id' => 12,
+                'id' => 17,
                 'name' => 'Sửa đơn hàng',
             ],
             [
-                'id' => 13,
+                'id' => 18,
                 'name' => 'Cập nhật trạng thái đơn hàng',
             ],
             [
-                'id' => 14,
+                'id' => 19,
                 'name' => 'Xem chi tiết đơn hàng',
             ],
 
             // Quản lý thanh trượt
             [
-                'id' => 15,
-                'name' => 'Xem danh sách thanh trượt',
-            ],
-            [
-                'id' => 16,
-                'name' => 'Thêm mới thanh trượt',
-            ],
-            [
-                'id' => 17,
-                'name' => 'Xem chi tiết thanh trượt',
-            ],
-            [
-                'id' => 18,
-                'name' => 'Sửa thanh trượt',
-            ],
-            [
-                'id' => 19,
-                'name' => 'Xóa thanh trượt',
-            ],
-
-            // Thương hiệu
-            [
                 'id' => 20,
-                'name' => 'Xem danh sách thương hiệu',
+                'name' => 'Xem danh sách slider',
             ],
             [
                 'id' => 21,
-                'name' => 'Thêm mới thương hiệu',
+                'name' => 'Thêm mới slider',
             ],
             [
                 'id' => 22,
-                'name' => 'Sửa thương hiệu',
+                'name' => 'Xem chi tiết slider',
             ],
-
-            // Hợp đồng
             [
                 'id' => 23,
-                'name' => 'Xem danh sách hợp đồng',
+                'name' => 'Sửa slider',
             ],
             [
                 'id' => 24,
+                'name' => 'Xóa slider',
+            ],
+
+            // Quản lý thương hiệu
+            [
+                'id' => 25,
+                'name' => 'Xem danh sách thương hiệu',
+            ],
+            [
+                'id' => 26,
+                'name' => 'Thêm mới thương hiệu',
+            ],
+            [
+                'id' => 27,
+                'name' => 'Sửa thương hiệu',
+            ],
+
+            // Quản lý hợp đồng
+            [
+                'id' => 28,
+                'name' => 'Xem danh sách hợp đồng',
+            ],
+            [
+                'id' => 29,
                 'name' => 'Thêm mới hợp đồng',
             ],
             [
-                'id' => 25,
+                'id' => 30,
                 'name' => 'Sửa hợp đồng',
             ],
 
             // Quản lý xe
             [
-                'id' => 26,
+                'id' => 31,
                 'name' => 'Xem danh sách xe',
             ],
             [
-                'id' => 27,
+                'id' => 32,
                 'name' => 'Thêm mới xe',
             ],
             [
-                'id' => 28,
-                'name' => 'Sửa xe',
+                'id' => 33,
+                'name' => 'Sửa thông tin xe',
             ],
 
             // Quản lý loại hợp đồng
             [
-                'id' => 29,
+                'id' => 34,
                 'name' => 'Xem danh sách loại hợp đồng',
             ],
             [
-                'id' => 30,
-                'name' => 'Thêm loại hợp đồng',
+                'id' => 35,
+                'name' => 'Thêm loại hợp đồng mới',
             ],
             [
-                'id' => 31,
+                'id' => 36,
                 'name' => 'Sửa loại hợp đồng',
             ],
 
-            // Khách hàng
+            // Quản lý khách hàng
             [
-                'id' => 32,
+                'id' => 37,
                 'name' => 'Xem danh sách khách hàng',
             ],
 
-            // Sản phẩm
+            // Quản lý sản phẩm
             [
-                'id' => 33,
+                'id' => 38,
                 'name' => 'Xem danh sách sản phẩm',
             ],
             [
-                'id' => 34,
+                'id' => 39,
                 'name' => 'Thêm mới sản phẩm',
             ],
             [
-                'id' => 35,
-                'name' => 'Sửa sản phẩm',
+                'id' => 40,
+                'name' => 'Sửa thông tin sản phẩm',
             ],
 
-            // Đơn hàng nhập
+            // Quản lý đơn hàng nhập
             [
-                'id' => 36,
+                'id' => 41,
                 'name' => 'Xem danh sách đơn nhập',
             ],
             [
-                'id' => 37,
-                'name' => 'Thêm mới đơn nhập',
-            ],
-            [
-                'id' => 38,
-                'name' => 'Sửa đơn nhập',
-            ],
-            [
-                'id' => 39,
-                'name' => 'Xem chi tiết đơn nhập',
-            ],
-            [
-                'id' => 40,
-                'name' => 'Yêu cầu hủy đơn nhập',
-            ],
-            [
-                'id' => 41,
-                'name' => 'Xem danh sách yêu cầu hủy',
-            ],
-            [
                 'id' => 42,
-                'name' => 'Hủy đơn nhập',
+                'name' => 'Thêm đơn nhập mới',
             ],
             [
                 'id' => 43,
-                'name' => 'Xác nhận đơn nhập',
+                'name' => 'Sửa đơn nhập',
             ],
             [
                 'id' => 44,
-                'name' => 'Tự động cập nhật trạng thái đơn nhập',
+                'name' => 'Xem chi tiết đơn nhập',
             ],
             [
                 'id' => 45,
-                'name' => 'Xem danh sách yêu cầu mới',
+                'name' => 'Yêu cầu hủy đơn nhập',
             ],
             [
                 'id' => 46,
-                'name' => 'Kiểm tra trạng thái đơn nhập',
+                'name' => 'Xem danh sách yêu cầu hủy',
             ],
             [
                 'id' => 47,
-                'name' => 'Cập nhật trạng thái đơn nhập',
+                'name' => 'Hủy đơn nhập',
+            ],
+            [
+                'id' => 48,
+                'name' => 'Xác nhận đơn nhập',
+            ],
+            [
+                'id' => 49,
+                'name' => 'Tự động cập nhật trạng thái',
+            ],
+            [
+                'id' => 50,
+                'name' => 'Xem danh sách yêu cầu mới',
+            ],
+            [
+                'id' => 51,
+                'name' => 'Kiểm tra trạng thái đơn',
+            ],
+            [
+                'id' => 52,
+                'name' => 'Cập nhật trạng thái đơn',
             ],
 
             // Quản lý đơn vị
             [
-                'id' => 48,
+                'id' => 53,
                 'name' => 'Xem danh sách đơn vị',
             ],
             [
-                'id' => 49,
+                'id' => 54,
                 'name' => 'Thêm mới đơn vị',
             ],
             [
-                'id' => 50,
+                'id' => 55,
                 'name' => 'Sửa đơn vị',
             ],
             [
-                'id' => 51,
+                'id' => 56,
                 'name' => 'Xóa đơn vị',
             ],
 
-            // Loại xe
+            // Quản lý loại xe
             [
-                'id' => 52,
+                'id' => 57,
                 'name' => 'Xem danh sách loại xe',
             ],
             [
-                'id' => 53,
+                'id' => 58,
                 'name' => 'Thêm mới loại xe',
             ],
             [
-                'id' => 54,
+                'id' => 59,
                 'name' => 'Sửa loại xe',
             ],
             [
-                'id' => 55,
+                'id' => 60,
                 'name' => 'Xóa loại xe',
             ],
 
-            // Danh mục
+            // Quản lý danh mục
             [
-                'id' => 56,
+                'id' => 61,
                 'name' => 'Xem danh sách danh mục',
             ],
             [
-                'id' => 57,
+                'id' => 62,
                 'name' => 'Thêm mới danh mục',
             ],
             [
-                'id' => 58,
+                'id' => 63,
                 'name' => 'Sửa danh mục',
             ],
             [
-                'id' => 59,
+                'id' => 64,
                 'name' => 'Xóa danh mục',
             ],
 
-            // Xếp hạng khách hàng
+            // Quản lý xếp hạng khách hàng
             [
-                'id' => 60,
+                'id' => 65,
                 'name' => 'Xem danh sách xếp hạng',
             ],
             [
-                'id' => 61,
+                'id' => 66,
                 'name' => 'Thêm mới xếp hạng',
             ],
             [
-                'id' => 62,
+                'id' => 67,
                 'name' => 'Sửa xếp hạng',
             ],
             [
-                'id' => 63,
+                'id' => 68,
                 'name' => 'Xóa xếp hạng',
             ],
 
             // Quản lý chuyến xe
             [
-                'id' => 64,
+                'id' => 69,
                 'name' => 'Xem danh sách chuyến xe',
             ],
             [
-                'id' => 65,
+                'id' => 70,
                 'name' => 'Thêm mới chuyến xe',
             ],
             [
-                'id' => 66,
+                'id' => 71,
                 'name' => 'Sửa chuyến xe',
             ],
             [
-                'id' => 67,
+                'id' => 72,
                 'name' => 'Xóa chuyến xe',
             ],
             [
-                'id' => 68,
+                'id' => 73,
                 'name' => 'Xem chi tiết chuyến xe',
             ],
         ]);
