3a0afdc89625d7b6ad3fd36f2520b31e7ad54dd2
DucNgoc2814 <phungducngoc28@gmail.com>
2024-11-11 22:20:28 +0700

    update

--- database/migrations/2024_09_09_164558_create_products_table.php
+++ database/migrations/2024_09_09_164558_create_products_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained('categories');
             $table->foreignId('unit_id')->constrained('units');
-            $table->foreignId('brand_id')->nullable()->constrained('brands');
+            $table->foreignId('brand_id')->constrained('brands');
             $table->string('slug', 255);
             $table->string('name', 255)->unique();
+            $table->string('image', 255)->nullable();
             $table->integer('price');
             $table->text('description')->nullable();
-            // $table->boolean('role')->default(true);
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
