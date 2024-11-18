84c01d7a085d16a957c81310760c5451cd1bda68
Nguyễn Quốc Tình <125818904+nguyentinhquoc@users.noreply.github.com>
2024-11-18 17:07:29 +0700

    Tinh commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,19 @@
 @extends('admin.layouts.master')
+@php
+if (!function_exists('formatCurrency')) {
+    /**
+     * Định dạng số thành tiền Việt Nam (VND)
+     *
+     * @param float $number
+     * @return string
+     */
+    function formatCurrency($number)
+    {
+        return number_format($number, 0, ',', '.') ;
+    }
+}
 
+@endphp
 @section('title')
     Dashboard
 @endsection
@@ -18,9 +32,8 @@
                     <div class="col-12">
                         <div class="d-flex align-items-lg-center flex-lg-row flex-column">
                             <div class="flex-grow-1">
-                                <h4 class="fs-16 mb-1">Good Morning, Anna!</h4>
-                                <p class="text-muted mb-0">Here's what's happening with your store
-                                    today.</p>
+                                <h4 class="fs-16 mb-1">Xin chào!</h4>
+                                <p class="text-muted mb-0">Chào mừng đến với thống kê trong tháng này của Gemo</p>
                             </div>
                             <div class="mt-3 mt-lg-0">
                                 <form action="javascript:void(0);">
@@ -62,22 +75,19 @@
                                 <div class="d-flex align-items-center">
                                     <div class="flex-grow-1 overflow-hidden">
                                         <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
-                                            Total Earnings</p>
+                                            Doanh thu</p>
                                     </div>
                                     <div class="flex-shrink-0">
-                                        <h5 class="text-success fs-14 mb-0">
-                                            <i class="ri-arrow-right-up-line fs-13 align-middle"></i>
-                                            +16.24 %
+                                        <h5 class="text-success fs-14 mb-0  {{ $growthRateRevenue < 0 ? 'text-danger' : ''  }} ">
+                                            <i class="ri-arrow-right-{{ $growthRateRevenue < 0 ? 'down' : 'up'  }}-line fs-13 align-middle "></i>
+                                            {{ $growthRateRevenue }} %
                                         </h5>
                                     </div>
                                 </div>
                                 <div class="d-flex align-items-end justify-content-between mt-4">
                                     <div>
-                                        <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value"
-                                                data-target="559.25">0</span>k
+                                        <h4 class="fs-22 fw-semibold ff-secondary mb-4">{{ formatCurrency( $totalRevenueThisMonth) }} đ
                                         </h4>
-                                        <a href="" class="text-decoration-underline">View net
-                                            earnings</a>
                                     </div>
                                     <div class="avatar-sm flex-shrink-0">
                                         <span class="avatar-title bg-success-subtle rounded fs-3">
@@ -89,6 +99,8 @@
                         </div><!-- end card -->
                     </div><!-- end col -->
 
+
+
                     <div class="col-xl-3 col-md-6">
                         <!-- card -->
                         <div class="card card-animate">
@@ -96,7 +108,38 @@
                                 <div class="d-flex align-items-center">
                                     <div class="flex-grow-1 overflow-hidden">
                                         <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
-                                            Orders</p>
+                                            Khách hàng</p>
+                                    </div>
+                                    <div class="flex-shrink-0">
+                                        <h5 class="text-success fs-14 mb-0  {{ $growthRateCustomers < 0 ? 'text-danger' : ''  }} ">
+                                            <i class="ri-arrow-right-{{ $growthRateCustomers < 0 ? 'down' : 'up'  }}-line fs-13 align-middle "></i>
+                                            {{ $growthRateCustomers }} %
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-end justify-content-between mt-4">
+                                    <div>
+                                        <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
+                                                data-target="183.35">0</span>M
+                                        </h4>
+                                    </div>
+                                    <div class="avatar-sm flex-shrink-0">
+                                        <span class="avatar-title bg-warning-subtle rounded fs-3">
+                                            <i class="bx bx-user-circle text-warning"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div><!-- end card body -->
+                        </div><!-- end card -->
+                    </div><!-- end col -->
+  <div class="col-xl-3 col-md-6">
+                        <!-- card -->
+                        <div class="card card-animate">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="flex-grow-1 overflow-hidden">
+                                        <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                                            Đơn hàng nhập</p>
                                     </div>
                                     <div class="flex-shrink-0">
                                         <h5 class="text-danger fs-14 mb-0">
@@ -110,8 +153,7 @@
                                         <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
                                                 data-target="36894">0</span>
                                         </h4>
-                                        <a href="" class="text-decoration-underline">View all
-                                            orders</a>
+
                                     </div>
                                     <div class="avatar-sm flex-shrink-0">
                                         <span class="avatar-title bg-info-subtle rounded fs-3">
@@ -122,7 +164,6 @@
                             </div><!-- end card body -->
                         </div><!-- end card -->
                     </div><!-- end col -->
-
                     <div class="col-xl-3 col-md-6">
                         <!-- card -->
                         <div class="card card-animate">
@@ -130,41 +171,7 @@
                                 <div class="d-flex align-items-center">
                                     <div class="flex-grow-1 overflow-hidden">
                                         <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
-                                            Customers</p>
-                                    </div>
-                                    <div class="flex-shrink-0">
-                                        <h5 class="text-success fs-14 mb-0">
-                                            <i class="ri-arrow-right-up-line fs-13 align-middle"></i>
-                                            +29.08 %
-                                        </h5>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-end justify-content-between mt-4">
-                                    <div>
-                                        <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
-                                                data-target="183.35">0</span>M
-                                        </h4>
-                                        <a href="" class="text-decoration-underline">See
-                                            details</a>
-                                    </div>
-                                    <div class="avatar-sm flex-shrink-0">
-                                        <span class="avatar-title bg-warning-subtle rounded fs-3">
-                                            <i class="bx bx-user-circle text-warning"></i>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div><!-- end card body -->
-                        </div><!-- end card -->
-                    </div><!-- end col -->
-
-                    <div class="col-xl-3 col-md-6">
-                        <!-- card -->
-                        <div class="card card-animate">
-                            <div class="card-body">
-                                <div class="d-flex align-items-center">
-                                    <div class="flex-grow-1 overflow-hidden">
-                                        <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
-                                            My Balance</p>
+                                            Đơn hàng bán</p>
                                     </div>
                                     <div class="flex-shrink-0">
                                         <h5 class="text-muted fs-14 mb-0">
@@ -177,8 +184,6 @@
                                         <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value"
                                                 data-target="165.89">0</span>k
                                         </h4>
-                                        <a href="" class="text-decoration-underline">Withdraw
-                                            money</a>
                                     </div>
                                     <div class="avatar-sm flex-shrink-0">
                                         <span class="avatar-title bg-primary-subtle rounded fs-3">
