5f522880f2fc74aeeb75b25ac4da3313de0c0550
TranManhThai98 <150803884+Nhamvuph42227@users.noreply.github.com>
2024-11-27 23:40:45 +0700

    update

--- routes/admin.php
+++ routes/admin.php
@@ -310,3 +310,4 @@ Route::middleware('CheckEmployees')->group(
 Route::post('/gui-giam-doc-pdf/{id}', [ContractController::class, 'sendToManagerPdf'])->name('contract.sendToManagerPdf');
 Route::get('hop-dong/xem-hop-dong/{id}/pdf', [ContractController::class, 'showPdf'])->name('showPdf');
 
+// aaa
